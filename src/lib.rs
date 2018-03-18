33811d1c932db1133df21f89c6dc9a10235b196f
Robert Panzer <Robert.Panzer.PB@googlemail.com>
2018-03-18 18:35:47 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+pub mod tree;
+
+#[cfg(test)]
+mod tests {
+
+    use super::tree::Tree;
+
+    #[test]
+    fn test_contains() {
+        let mut tree: Tree<String> = Tree::new();
+        tree.insert(String::from("C"));
+        tree.insert(String::from("B"));
+        tree.insert(String::from("D"));
+        assert!(!tree.contains(&String::from("A")));
+        assert!(tree.contains(&String::from("B")));
+        assert!(tree.contains(&String::from("C")));
+        assert!(tree.contains(&String::from("D")));
+        assert!(!tree.contains(&String::from("E")));
+    }
+
+    #[test]
+    fn remove_node_with_two_children() {
+        let mut tree: Tree<String> = Tree::new();
+        tree.insert(String::from("B"));
+        tree.insert(String::from("A"));
+        tree.insert(String::from("C"));
+        println!("Tree: {:?}", tree);
+        let old_size = tree.size();
+        assert_eq!(Some(String::from("B")), tree.delete(&String::from("B")));
+        assert_eq!(old_size - 1, tree.size());
+        println!("Tree: after delete {:?}", tree);
+    }
+
+    #[test]
+    fn remove_unknown_node() {
+        let mut tree: Tree<String> = Tree::new();
+        tree.insert(String::from("B"));
+        tree.insert(String::from("A"));
+        tree.insert(String::from("C"));
+        println!("Tree: {:?}", tree);
+        let old_size = tree.size();
+        assert_eq!(None, tree.delete(&String::from("D")));
+        assert_eq!(old_size, tree.size());
+        println!("Tree: after delete {:?}", tree);
+    }
+
+    #[test]
+    fn remove_from_empty_tree() {
+        let mut tree: Tree<String> = Tree::new();
+        println!("Tree: {:?}", tree);
+        assert_eq!(0, tree.size());
+        assert_eq!(None, tree.delete(&String::from("D")));
+        assert_eq!(0, tree.size());
+        println!("Tree: after delete {:?}", tree);
+    }
+}
---
+++ src/tree.rs
@@ -0,0 +1,163 @@
+use std::cmp::Ordering::*;
+use std::mem;
+
+#[derive(Debug)]
+pub struct Tree<T> {
+    root: Link<T>
+}
+
+#[derive(Debug)]
+enum Link<T> {
+    Empty,
+    NonEmpty(Box<Node<T>>),
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    value: T,
+    left: Link<T>,
+    right: Link<T>,
+}
+
+impl<T: Ord> Tree<T> {
+    pub fn new() -> Self {
+        Tree { root: Link::Empty }
+    }
+
+    pub fn insert(&mut self, value: T) {
+        self.root.insert(value)
+    }
+
+    pub fn delete(&mut self, value: &T) -> Option<T> {
+        self.root.delete(value)
+    }
+
+    pub fn size(&self) -> usize {
+        self.root.size()
+    }
+
+    pub fn contains(&self, value: &T) -> bool {
+        self.root.contains(value)
+    }
+}
+
+impl<T: Ord> Link<T> {
+    fn insert(&mut self, value: T) {
+        match *self {
+            Link::Empty =>
+                *self = Link::NonEmpty(Box::new(Node { value, left: Link::Empty, right: Link::Empty })),
+            Link::NonEmpty(ref mut boxed_node) =>
+                match value.cmp(&boxed_node.value) {
+                    Equal => (),
+                    Greater => boxed_node.right.insert(value),
+                    Less => boxed_node.left.insert(value),
+                }
+        }
+    }
+
+    fn delete(&mut self, value: &T) -> Option<T> {
+        match mem::replace(self, Link::Empty) {
+            Link::Empty => None,
+            Link::NonEmpty(mut boxed_node) => {
+                match value.cmp(&boxed_node.value) {
+                    Equal => {
+                        // self points to the node to delete
+                        if boxed_node.is_leaf() {
+                            // Easy, node is a leaf, just return the value, self is already replaced with Empty
+                            Some(boxed_node.value)
+                        } else if boxed_node.has_left_child() && !boxed_node.has_right_child() {
+                            // Node has no right child, just let this Link point to the left child
+                            *self = mem::replace(&mut boxed_node.left, Link::Empty);
+                            Some(boxed_node.value)
+                        } else if !boxed_node.has_left_child() && boxed_node.has_right_child() {
+                            // Node has no left child, just let this Link point to the right child
+                            *self = mem::replace(&mut boxed_node.right, Link::Empty);
+                            Some(boxed_node.value)
+                        } else {
+                            // The most complex case, delete the minimum node in the right subtree and
+                            // replace its value in this node
+                            let min_node = boxed_node.right.delete_min().unwrap();
+                            let old_value = mem::replace(&mut boxed_node.value, min_node.value);
+                            *self = Link::NonEmpty(boxed_node);
+                            Some(old_value)
+                        }
+                    }
+                    Less => {
+                        // The value to delete is in the left subtree -> recurse
+                        let result = boxed_node.left.delete(value);
+                        *self = Link::NonEmpty(boxed_node);
+                        result
+                    }
+                    Greater => {
+                        // The value to delete is in the right subtree -> recurse
+                        let result = boxed_node.right.delete(value);
+                        *self = Link::NonEmpty(boxed_node);
+                        result
+                    }
+                }
+            }
+        }
+    }
+
+    //
+    // A helper function that is called by delete() to delete the minimum node in the right subtree
+    // when the the node to delete has two children.
+    fn delete_min(&mut self) -> Option<Box<Node<T>>> {
+        match mem::replace(self, Link::Empty) {
+            Link::Empty => None,
+            Link::NonEmpty(mut boxed_node) => {
+                if boxed_node.is_leaf() {
+                    Some(boxed_node)
+                } else if boxed_node.has_left_child() {
+                    let result = boxed_node.left.delete_min();
+                    *self = Link::NonEmpty(boxed_node);
+                    result
+                } else {
+                    // Must have a right child
+                    *self = mem::replace(&mut boxed_node.right, Link::Empty);
+                    Some(boxed_node)
+                }
+            }
+        }
+    }
+
+    fn size(&self) -> usize {
+        match *self {
+            Link::Empty => 0,
+            Link::NonEmpty(ref node) => 1 + node.left.size() + node.right.size()
+        }
+    }
+
+    fn empty_tree(&self) -> bool {
+        match *self {
+            Link::Empty => true,
+            _ => false
+        }
+    }
+
+    fn contains(&self, value: &T) -> bool {
+        match *self {
+            Link::Empty => false,
+            Link::NonEmpty(ref node) =>
+                match value.cmp(&node.value) {
+                    Equal => true,
+                    Less => node.left.contains(value),
+                    Greater => node.right.contains(value)
+                }
+        }
+    }
+}
+
+impl<T: Ord> Node<T> {
+    fn is_leaf(&self) -> bool {
+        self.left.empty_tree() && self.right.empty_tree()
+    }
+
+    fn has_left_child(&self) -> bool {
+        !self.left.empty_tree()
+    }
+
+    fn has_right_child(&self) -> bool {
+        !self.right.empty_tree()
+    }
+}
